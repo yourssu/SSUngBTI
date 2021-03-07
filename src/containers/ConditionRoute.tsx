3fcaa3c348f2508d9a46acade7f09da8df79c486
Jisu Kim <webmaster@alien.moe>
2021-03-08 00:46:03 +0900

    Fix typo

--- src/containers/ConditionRoute.tsx
+++ src/containers/ConditionRoute.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 import { Route, RouteComponentProps, RouteProps } from "react-router-dom";
 
-type RouteRequireProps = RouteProps & {
+type ConditionRouteProps = RouteProps & {
   condition: (props: RouteComponentProps) => boolean;
   failed: JSX.Element;
 };
-export const ConditionRoute: FC<RouteRequireProps> = ({
+export const ConditionRoute: FC<ConditionRouteProps> = ({
   condition,
   failed,
   children,
